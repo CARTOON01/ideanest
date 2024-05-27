9e4d249591437973508d679d710e526bda6789ab
CARTOON <brianngugi224@gmail.com>
2024-05-27 04:44:51 +0300

    Added trash to navigation for recovery of deleted items

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -7,7 +7,8 @@ import {
     Plus,
     PlusCircle,
     Search,
-    Settings
+    Settings,
+    Trash
 } from "lucide-react";
 import { usePathname } from "next/navigation";
 import { ElementRef, useEffect, useRef, useState } from "react";
@@ -18,6 +19,11 @@ import { api } from "@/convex/_generated/api";
 import { Item } from "./item";
 import { toast } from "sonner";
 import { DocumentList } from "./document-list";
+import {
+    Popover,
+    PopoverContent,
+    PopoverTrigger
+} from "@/components/ui/popover";
 
 export const Navigation = () => {
     const pathname = usePathname();
@@ -161,6 +167,17 @@ export const Navigation = () => {
                         icon={Plus}
                         label="Add a page"
                     />
+                    <Popover>
+                        <PopoverTrigger className="w-full mt-4">
+                            <Item label="Trash" icon={Trash} />
+                        </PopoverTrigger>
+                        <PopoverContent
+                            className="p-0 w-72"
+                            side={isMobile ? "bottom" : "right"}
+                        >
+                            <p>Trash Box</p>
+                        </PopoverContent>
+                    </Popover>
                 </div>
                 <div
                     onMouseDown={handleMouseDown}
