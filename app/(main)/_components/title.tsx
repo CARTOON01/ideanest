1e6debf76e9cfd35a94d54711b71b38cb7e2fde9
CARTOON <brianngugi224@gmail.com>
2024-05-28 17:53:44 +0300

    Add lazy loading for documents in navbar and title components

--- app/(main)/_components/title.tsx
+++ app/(main)/_components/title.tsx
@@ -1,6 +1,12 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Skeleton } from "@/components/ui/skeleton";
+import { api } from "@/convex/_generated/api";
 import { Doc } from "@/convex/_generated/dataModel";
+import { useMutation } from "convex/react";
+import { useRef, useState } from "react";
 
 interface TitleProps {
     initialData: Doc<"documents">,
@@ -9,9 +15,74 @@ interface TitleProps {
 export const Title = ({
     initialData
 }: TitleProps) => {
+    const inputref = useRef<HTMLInputElement>(null);
+    const update = useMutation(api.documents.update);
+
+    const [title, setTitle] = useState(initialData.title || "New Document");
+    const [isEditing, setIsEditing] = useState(false);
+
+    const enableInput = () => {
+        setTitle(initialData.title);
+        setIsEditing(true);
+        setTimeout(() => {
+            inputref.current?.focus();
+            inputref.current?.setSelectionRange(0, inputref.current?.value.length)
+        }, 0);
+    };
+
+    const disableInput = () => {
+        setIsEditing(false);
+    };
+
+    const onChange = (
+        event: React.ChangeEvent<HTMLInputElement>
+    ) => {
+        setTitle(event.target.value);
+        update({
+            id: initialData._id,
+            title: event.target.value || "New Document"
+        })
+    };
+
+    const onKeyDown = (
+        event: React.KeyboardEvent<HTMLInputElement>
+    ) => {
+        if (event.key === "Enter") {
+            disableInput();
+        }
+    };
+
     return (
-        <div>
-            Title
+        <div className="flex items-center gap-x-1">
+            {!!initialData.icon && <p>{initialData.icon}</p>}
+            {isEditing ? (
+                <Input
+                    ref={inputref}
+                    onClick={enableInput}
+                    onBlur={disableInput}
+                    onChange={onChange}
+                    onKeyDown={onKeyDown}
+                    value={title}
+                    className="h-7 px-2 focus-visible:ring-transparent"
+                />
+            ) : (
+                <Button
+                    onClick={enableInput}
+                    variant="ghost"
+                    size="sm"
+                    className="font-normal h-auto p-1"
+                >
+                    <span className="truncate">
+                        {initialData?.title}
+                    </span>
+                </Button>
+            )}
         </div>
     )
 }
+
+Title.Skeleton = function TitleSkeleton () {
+    return (
+        <Skeleton h-9 w-16 rounded-md />
+    );
+};
