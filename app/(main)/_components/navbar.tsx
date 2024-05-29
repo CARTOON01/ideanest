cfcd0f03bd1e7ae6d54e9bd63888d9e6c1b6f8ad
CARTOON <brianngugi224@gmail.com>
2024-05-29 14:42:53 +0300

    Add menu component for document actions

--- app/(main)/_components/navbar.tsx
+++ app/(main)/_components/navbar.tsx
@@ -7,6 +7,7 @@ import { MenuIcon } from "lucide-react";
 import { useParams } from "next/navigation";
 import { Title } from "./title";
 import { Banner } from "./banner";
+import { Menu } from "./menu";
 
 interface NavbarProps {
     isCollapsed: boolean;
@@ -47,6 +48,9 @@ export const Navbar = ({
                 )}
                 <div className="flex items-center justify-between w-full">
                     <Title initialData={document} />
+                    <div className="flex items-center gap-x-2">
+                        <Menu documentId={document._id} />
+                    </div>
                 </div>
             </nav>
             {document.isArchived && (
