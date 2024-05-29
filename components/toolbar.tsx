dd3fdf0b86e652a4d899dcadf159b526dfc4adcd
CARTOON <brianngugi224@gmail.com>
2024-05-29 16:11:31 +0300

    Add IconPicker component for selecting icons

---
+++ components/toolbar.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { Doc } from "@/convex/_generated/dataModel";
+import { IconPicker } from "./icon-picker";
+
+interface ToolbarProps {
+    initialData: Doc<"documents">;
+    preview?: boolean;
+};
+
+export const Toolbar = ({
+    initialData,
+    preview
+}: ToolbarProps) => {
+    return (
+        <div className="pl-[54px] group relative">
+            {!!initialData.icon && !preview && (
+                <div className="flex items-center gap-x-2 group/icon ">
+                    <IconPicker onchange={() => {}}>
+                        <p className="text-6xl hover:opacity-75 transition">
+                            {initialData.icon}
+                        </p>
+                    </IconPicker>
+                </div>
+            )}
+        </div>
+    )
+}
