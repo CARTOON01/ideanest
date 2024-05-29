5c63b98269081c56708155d6eb65cbec983fb320
CARTOON <brianngugi224@gmail.com>
2024-05-29 20:49:37 +0300

    Add button to change and remove cover image in Cover component

--- components/cover.tsx
+++ components/cover.tsx
@@ -3,6 +3,9 @@
 import Image from "next/image";
 
 import { cn } from "@/lib/utils";
+import { Button } from "./ui/button";
+import { ImageIcon, X } from "lucide-react";
+import { useCoverImage } from "@/hooks/use-cover-image";
 
 interface CoverImageProps {
     url?: string;
@@ -13,6 +16,8 @@ export const Cover = ({
     url,
     preview
 }: CoverImageProps) => {
+    const coverImage = useCoverImage();
+
     return (
         <div className={cn(
             "relative w-full h-[35vh] group",
@@ -26,6 +31,28 @@ export const Cover = ({
                     alt="Cover"
                     className="object-cover"
                 />
+            )}
+            {url && !preview && (
+                <div className="opacity-0 group-hover:opacity-100 absolute bottom-5 right-5 flex items-center gap-x-2">
+                    <Button
+                        onClick={coverImage.onOpen}
+                        className="text-muted-foreground text-xs"
+                        variant="outline"
+                        size="sm"
+                    >
+                        <ImageIcon />
+                        Change Cover
+                    </Button>
+                    <Button
+                        onClick={() => {}}
+                        className="text-muted-foreground text-xs"
+                        variant="outline"
+                        size="sm"
+                    >
+                        <X />
+                        Remove Cover
+                    </Button>
+                </div>
             )}
         </div>
     )
