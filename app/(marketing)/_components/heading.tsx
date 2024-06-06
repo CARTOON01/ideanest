2a097cffce9fd4458f5a34cc3811e5f66db1757b
CARTOON <brianngugi224@gmail.com>
2024-06-06 03:58:21 +0300

    hot fix

--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -12,10 +12,10 @@ export const Heading = () => {
     return (
         <div className="max-w-3xl space-y-4">
             <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
-                Your Ideas, Documents & Plans. Unified. Welcome to <span className="underline">IdeaNist</span>
+                Your Ideas, Documents & Plans. Unified. Welcome to <span className="underline">IdeaNest</span>
             </h1>
             <h3 className="text-base sm:text-xl md:text-3xl font-medium">
-                IdeaNist is a Workspace where <br />
+                IdeaNest is a Workspace where <br />
                 better, faster work happens
             </h3>
             {isLoading &&(
